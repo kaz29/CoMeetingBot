65f3ae3a41f23e1753426ef3826bb30001978cea
Kaz Watanabe <cyo@mac.com>
2013-03-12 11:27:36 +0900

    Jenkins notify format update

--- lib/JenkinsNotify.php
+++ lib/JenkinsNotify.php
@@ -6,21 +6,27 @@ class JenkinsNotify extends NotifyBase
     protected function requestToNotifyData()
     {
         $request = $this->loadRequest();
-        if ($request->build->phase !== 'FINISHED') {
+        if ($request === false) {
             return false;
         }
 
-        if (!isset($request->build->status)) {
-            return false;
-        } 
+        if (isset($_GET['force']) !== true) {
+            if ($request->build->phase !== 'FINISHED') {
+                return false;
+            }
 
-        if ($request->build->status === 'SUCCESS') {
-            return false;
-        } 
+            if (!isset($request->build->status)) {
+                return false;
+            }
+
+            if ($request->build->status === 'SUCCESS') {
+                return false;
+            }
+        }
 
         $data = array(
             'link' => $request->build->full_url,
-            'name' => "{$request->name} build {$request->build->status}",
+            'name' => "Project:{$request->name} BUILD-{$request->build->phase}:{$request->build->status}",
         );
 
         $data['body'] = $data['name'].str_repeat(' ', 300).$data['link'];
@@ -30,7 +36,20 @@ class JenkinsNotify extends NotifyBase
     
     private function loadRequest()
     {
-        if( isset($GLOBALS['HTTP_RAW_POST_DATA']) ) {
+        /*
+        $response = new StdClass();
+        $response->build = new StdClass();
+
+        $response->build->phase = 'START';
+        $response->build->status = 'FINISHED';
+        $response->name = 'TEST';
+        $response->build->full_url = 'http://examle.com/';
+
+        return $response;
+        */
+        
+        $requests = false;
+        if (isset($GLOBALS['HTTP_RAW_POST_DATA'])) {
             $requests = json_decode(urldecode($GLOBALS['HTTP_RAW_POST_DATA']));
             $result = json_last_error();
             if ( $result !== JSON_ERROR_NONE ) {
